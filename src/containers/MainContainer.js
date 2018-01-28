22753d9f7f6e118612f4bb35d1dc36c4ae29df6c
jasan_s <usasikh1@yahoo.com>
2018-01-28 03:52:17 -0800

    added loader to display fetching

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-export default class MainContainer extends Component {
+import { Dimmer, Loader} from 'semantic-ui-react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as ActionCreators from 'redux/modules/main'
+import { withRouter } from 'react-router-dom'
+import styled from 'styled-components'
+
+class MainContainer extends Component {
   static propTypes = {
     children: PropTypes.array.isRequired,
   }
@@ -11,14 +18,39 @@ export default class MainContainer extends Component {
     super(props)
     this.state = {}
   }
-
-
-
   render() {
     return (
       <div>
+      {this.props.fetching
+        ? <StyledLoader active size='massive'></StyledLoader>
+        : null }
         {this.props.children}
       </div>
     )
   }
 }
+
+
+function mapStateToProps ({main}, props) {
+  const {fetching} = main
+  return {
+    fetching,
+  }
+}
+
+function mapDispatchToProps (dispatch, props) {
+  return bindActionCreators(ActionCreators, dispatch)
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MainContainer))
+
+const StyledLoader = styled(Loader)`
+  width: 100%
+  height: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  z-index: 999;
+`
