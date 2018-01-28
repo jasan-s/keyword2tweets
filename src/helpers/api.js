2882dd0a5de8ca97026b07d57cde0390cd9a1817
jasan_s <usasikh1@yahoo.com>
2018-01-28 01:28:51 -0800

    added firebase config | normalizing keyword by trim and lowercase | added keyword saving to firebase

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import {firebaseRef} from 'config/constants'
 
-export default async function fetchTweetsGivenKeyword(keyword) {
+export async function fetchTweetsGivenKeyword(keyword) {
   try {
     const response = await axios.post(`https://us-central1-keyword2tweets.cloudfunctions.net/twitterKeyword2Tweets`, {keyword: keyword})
      process.env.NODE_ENV === 'production' ? null : console.log('fetchTweetsGivenKeyword response from cloud function: ', response)
@@ -9,5 +10,26 @@ export default async function fetchTweetsGivenKeyword(keyword) {
       process.env.NODE_ENV === 'production' ? null : console.log('fetchTweetsGivenKeyword error from cloud function: ', error.response)
       return error.response
   }
+
+}
+
+
+export async function saveSearchedKeywordtoFirebase(keyword) {
+  try {
+  const createdAtTimeStamp = (Date.now() * -1)
+  const keywordId = firebaseRef.child('keywords').push().key
+ 
+
+  const keywordData = {keyword, createdAtTimeStamp, keywordId}
+
+  const updates = {}
+
+  // // save data to keyword endpoint
+  updates[`/keywords/${keywordId}`] = keywordData
+  return firebaseRef.update(updates)
+} catch(error) {
+  process.env.NODE_ENV === 'production' ? null : console.log('saveNewCampaignPost error :', error)
+  return error
+}
 
 }
