0233711467051a2d9f6ca9253140caad17ad61cf
jasan_s <usasikh1@yahoo.com>
2018-01-27 22:42:28 -0800

    added submit button w/ thunk trigger | added header and subheaders to home with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Helmet} from 'react-helmet'
 import { Input } from 'semantic-ui-react'
+import styled from 'styled-components'
 
 export default function Home(props) {
-  const {userEnteredKeyword, handleInputChange} = props
+  const {userEnteredKeyword, handleInputChange, handleSearchSubmit} = props
   return (
     <div>
         <Helmet>
@@ -19,8 +20,13 @@ export default function Home(props) {
           <meta name='twitter:title' content='Keyword2Tweets – Given a keyword, retrieves 10 most recet tweets from twitter.'/>
           <meta name='twitter:description' content='The App allows user to enter in a keyword retrieve 10 most recent tweets(using Twitter API) if they exist.'/>
       </Helmet>
-        <Input className='search-box' value= {userEnteredKeyword} icon='search'
-        onChange={handleInputChange} placeholder='Enter Keyword...' />
+      <StyledSearchDiv>
+        <StyledLargeHeader>Keyword2Tweets</StyledLargeHeader>
+        <StyledSubHeader>Enter keyword to see the most recent tweets matching the keyword</StyledSubHeader>
+        <StyledInput className='search-box' value= {userEnteredKeyword}
+        onChange={handleInputChange} placeholder='Keyword...' />
+        <Button onClick={handleSearchSubmit} >Search</Button>
+        </StyledSearchDiv>
 
     </div>
   )
@@ -29,8 +35,63 @@ export default function Home(props) {
 Home.propTypes = {
   userEnteredKeyword: PropTypes.string.isRequired,
   handleInputChange: PropTypes.func.isRequired,
+  handleSearchSubmit: PropTypes.func.isRequired,
 }
 
 Home.defaultProps = {
   userEnteredKeyword: '',
 }
+
+
+const StyledSearchDiv = styled('div')`
+  margin-top: 12vh;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+
+`
+
+const StyledInput = styled(Input)`
+width: 320px;
+max-width: 92vw;
+margin-bottom: 2vh;
+border-radius: 2px;
+box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
+transition: box-shadow 200ms cubic-bezier(0.4, 0.0, 0.2, 1);
+&:hover {
+  box-shadow: 0 3px 8px 0 rgba(0,0,0,0.2), 0 0 0 1px rgba(0,0,0,0.08);
+}
+`
+
+const Button = styled.button`
+  border-radius: 3px;
+  background: white;
+  color: grey;
+  padding: 0.5em 0.5em;
+  width: 136px;
+  border: 1px solid grey;
+  cursor: pointer;
+  &:hover {
+  border: 2px solid grey;
+  background: grey;
+  color: white;}
+`
+
+
+const StyledLargeHeader = styled('div')`
+  font-size: 2em;
+  font-weight: 800;
+  margin-bottom: 2vh;
+`
+
+const StyledSubHeader = styled('div')`
+  font-size: 1.2em;
+  font-weight: 500;
+  margin-bottom: 5vh;
+  @media (max-width:420px) {
+   font-size: 0.86em;
+  } 
+
+`
